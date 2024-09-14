b184bbfda2375158c701c4756298e2fc552151e0
Prranjali Patel <prranjali.wiz@gmail.com>
2024-09-14 21:25:58 +0530

    Added pages

---
+++ frontend/src/assets/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="bg-red-400 text-red-950">
+      <h1 className="text-3xl font-bold underline text-red-500 border border-red-700">
+      Hello world! 
+     </h1>
+    
+    </div>
+  )
+}
+
+export default Home
